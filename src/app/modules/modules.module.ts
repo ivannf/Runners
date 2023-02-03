116ccc3c6220a4a735251f3cff80a4574b8e5373
= <=>
2023-02-03 12:42:57 +0100

    Runners

---
+++ src/app/modules/modules.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    CommonModule  
+  ],
+  exports: [
+    
+  ]
+})
+export class ModulesModule { }
