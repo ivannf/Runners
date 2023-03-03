d5c09af1d8c9b3cb22c57b9b799323f49ca5e3e9
= <=>
2023-03-03 13:55:01 +0100

    Cambios registro evento

--- src/app/modules/registro-evento/registro-evento.component.ts
+++ src/app/modules/registro-evento/registro-evento.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-registro-evento',
   templateUrl: './registro-evento.component.html',
   styleUrls: ['./registro-evento.component.scss']
 })
-export class RegistroEventoComponent {
 
+export class RegistroEventoComponent {
+  dniFormControl = new FormControl('', [Validators.required, Validators.pattern('^[0-9]{8}[A-HJ-NP-TV-Z]$')]);
+
+  nombreFormControl = new FormControl('', Validators.required);
+
+  apellidosFormControl = new FormControl('', Validators.required);
+
+  direccionFormControl = new FormControl('', Validators.required);
+
+  clubRunningFormControl = new FormControl('', Validators.required);
+
+  tallaFormControl = new FormControl('', [Validators.required, Validators.pattern('^(XS|S|M|L|XL)$')]);
+
+  metodoFormControl = new FormControl('', Validators.required);
+
+  matcher = new MyErrorStateMatcher();
 }
